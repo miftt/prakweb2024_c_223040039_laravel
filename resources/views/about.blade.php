5b2a0eb18a8da6926722bdf5bda2162a03f60be2
miftt <miftahfauzi012@gmail.com>
2024-10-22 17:55:31 +0700

    Struktur Folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman About</h1>
+    <h3>Owner: {{ $nama }}</h3>
+    <img src="img/1.jpg" alt="" width="200">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Miftah Fauzi']);
+});
+
+Route::get('/', function () {
+    return view('home');
 });
