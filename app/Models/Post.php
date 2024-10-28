060377f3cd75e830aef818dce069d156649a6954
miftt <miftahfauzi012@gmail.com>
2024-10-28 22:07:59 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,36 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Miftah Fauzi',
-                'body' =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat ratione architecto error dignissimos neque et, suscipit quasi nemo. Beatae nisi est non eos?',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Miftah Fauzi',
-                'body' =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptate laborum architecto, sapiente aut dignissimos unde ducimus aliquam labore consectetur adipisci voluptatem, nisi quisquam doloribus?',
-            ]
-        ];
-    }
-    public static function find($slug): array
-    {
-
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-        if (! $post) {
-            abort(404);
-        }
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
   <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
   <h2 class=" mb-1 text-3xl tracking-tight font-bold text-gray-900"> {{ $post ['title'] }} </h2>
   </a>
-  <div class="text-base text-gray-500"><a href="#">{{ $post ['author'] }}</a> | 28 Oktober 2024 </div>
+  <div class="text-base text-gray-500"><a href="#">{{ $post ['author'] }}</a> | {{ $post->created_at->diffForHumans() }}</div>
     <p class="my-4 font-light">
       {{ Str::limit($post ['body'], 150) }}
     </p>
